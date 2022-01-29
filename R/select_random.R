7bc9fc3748ec5160386a7367269a093b9c831ebc
b-c-r <bjoern.c.rall@gmail.com>
2022-01-29 13:07:43 +0100

    Updated license information and description

--- R/select_random.R
+++ R/select_random.R
@@ -1,3 +1,19 @@
+# bcrtools: a little R-function collection for ecological statistics
+# Copyright (C) 2022  Bjoern C. Rall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #' Automatically select the random structure using "lme"
 #'
 #' @param dat The data to use. Should be a data frame.
